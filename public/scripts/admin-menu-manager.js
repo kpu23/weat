06238c386eeaf1e0abb56da3b85951ca738a3a93
bsaff <bsaff4@gmail.com>
2016-03-14 11:00:50 -0400

    bug fixes for menu management with refactor of code

--- public/scripts/admin-menu-manager.js
+++ public/scripts/admin-menu-manager.js
@@ -1,51 +1,26 @@
 var restaurantId = "56c503be9bc2f4cc1396845e"; //HACK: fetch from logged-in account
 
-// Templates for new objects
-var Menu = {
-    name: '',
-    isPublic: false,
-    restaurantId: restaurantId,
-    menuCategories: []
-};
-
-var Category = {
-    "id": ko.observable(),
-    "name" : '',
-    "description" : '',
-    "imgPath" : '',
-    "restaurantId" : restaurantId,
-    "foodItems" : ko.observableArray()
-};
-
-var FoodItem = {
-    "name" : '',
-    "price" : '',
-    "available" : '',
-    "description" : '',
-    "imgPath" : '',
-    "averagePrepTime" : '', // in minutes
-    "restaurantId" : restaurantId
-};
-
 var MenuManagementModel = function() {
     var self = this;
     self.currentMenuName = ko.observable();
     self.currentCategoryName = ko.observable();
     self.menus = ko.observableArray();
-    self.newMenu = ko.observable(Menu);
-    self.newCategory = ko.observable(Category);
-    self.newFoodItem = ko.observable(FoodItem);
+    self.newMenu = ko.observable(new MenuModel());
+    self.newCategory = ko.observable(new CategoryModel());
+    self.newFoodItem = ko.observable(new FoodItemModel());
     self.currentMenu = ko.observable();
     self.currentCategory = ko.observable();
     self.fetchMenus = function() {
         $.post("/admin/fetchMenus", {restaurantId: restaurantId}, function(menus){
             $('#menu-manager').fadeIn();
-            self.menus(menus);
+            menus.forEach(function (menu) {
+                self.menus.push(new MenuModel(menu));
+            });
         });
     };
 
     self.createMenu = function() {
-        if (self.newMenu().name) {
+        if (self.newMenu().name()) {
             console.log(ko.toJSON(self.newCategory()));
             console.log(ko.toJSON(self.newMenu()));
 
@@ -55,9 +30,9 @@ var MenuManagementModel = function() {
                     alert('Error occurred. Please contact us at help@weat.com.')
                 } else {
                     $('#create-menu-window').modal('hide')
-                    self.newMenu()._id = result.menuId;
-                    console.log(self.newMenu());
+                    self.newMenu().id(result.menuId);
                     self.menus.push(self.newMenu());
+                    self.newMenu(new MenuModel());
                 }
             });
         } else {
@@ -70,19 +45,18 @@ var MenuManagementModel = function() {
             if (result.error) {
                 alert('Error occurred. Please contact us at help@weat.com.')
             } else {
-                $('#edit-menu-window').modal('hide')
+                $('#edit-menu-window').modal('hide');
                 self.menus.remove(function (item) {
-                    return item._id == self.currentMenu().id();
+                    return item.id() == self.currentMenu().id();
                 });
-                self.currentMenu('');
-                self.currentCategory('');
+                self.showAllMenus();
             }
         });
     };
     self.createCategory = function() {
         var currentMenuId = self.currentMenu().id();
         console.log(self.currentMenu());
-        if (self.newCategory().name) {
+        if (self.newCategory().name()) {
             $.post("/admin/createCategory", {category: ko.toJSON(self.newCategory()), menuId: currentMenuId.toString()}, function(result){
                 console.log(result);
                 if (result.error) {
@@ -91,6 +65,7 @@ var MenuManagementModel = function() {
                     $('#create-category-window').modal('hide');
                     self.newCategory().id(result.id);
                     self.currentMenu().categories.push(self.newCategory());
+                    self.newCategory(new CategoryModel());
                 }
             });
         } else {
@@ -100,14 +75,16 @@ var MenuManagementModel = function() {
     self.createFoodItem = function() {
         var currentCategoryId = self.currentCategory().id();
         console.log(self.currentMenu());
-        if (self.newFoodItem().name) {
-            $.post("/admin/createFoodItem", {foodItem: ko.toJSON(self.newFoodItem()), categoryId: currentCategoryId.toString()}, function(result){
+        if (self.newFoodItem().name()) {
+            $.post("/admin/createFoodItem", {foodItem: ko.toJSON(self.newFoodItem), categoryId: currentCategoryId.toString()}, function(result){
                 console.log(result);
                 if (result.error) {
                     alert('Error occurred. Please contact us at help@weat.com.')
                 } else {
                     $('#create-food-item-window').modal('hide')
+                    //self.newFoodItem().id(result.id);
                     self.currentCategory().foodItems.push(self.newFoodItem());
+                    self.newFoodItem(new FoodItemModel());
                 }
             });
         } else {
@@ -133,13 +110,12 @@ var MenuManagementModel = function() {
         $('#all-menus').hide();
         self.currentCategory('');
         self.currentCategoryName('');
-        self.currentMenuName(menu.name);
-        self.currentMenu(new MenuModel(menu));
+        self.currentMenuName(menu.name());
+        self.currentMenu(menu);
         $('#all-categories').fadeIn();
     };
     self.showCategoryEditor = function(category) {
         $('#all-categories').hide();
-        console.log(category);
         self.currentCategoryName(category.name());
         self.currentCategory(category);
         $('#food-item-manager').fadeIn();
@@ -154,10 +130,10 @@ $(document).ready(function(){
 
 function MenuModel(menu){
     var self = this;
-    self.id = ko.observable(menu._id);
-    self.name = ko.observable(menu.name);
-    self.isPublic = ko.observable(menu.isPublic);
-    self.restaurantId = ko.observable(menu.restaurantId);
+    self.id = ko.observable();
+    self.name = ko.observable();
+    self.isPublic = ko.observable();
+    self.restaurantId = ko.observable();
     self.categories = ko.observableArray();
     self.fetchCategories = function(ids) {
         $.post("/admin/fetchCategories", {categoryIds: JSON.stringify(ids)}, function(categories){
@@ -167,30 +143,63 @@ function MenuModel(menu){
         });
     };
     // Initialize
-    if (menu.menuCategories.length > 0) {
-        self.fetchCategories(menu.menuCategories);
+    if (menu) {
+        self.id(menu._id);
+        self.name(menu.name);
+        self.isPublic(menu.isPublic);
+        self.restaurantId(menu.restaurantId);
+        if (menu.menuCategories.length > 0) {
+            self.fetchCategories(menu.menuCategories);
+        }
     }
 }
 
 function CategoryModel(category){
     var self = this;
-    self.id = ko.observable(category._id);
-    self.name = ko.observable(category.name);
-    self.description = ko.observable(category.description);
-    self.restaurantId = ko.observable(category.restaurantId);
+    self.id = ko.observable();
+    self.name = ko.observable();
+    self.description = ko.observable();
+    self.restaurantId = ko.observable();
     self.foodItems = ko.observableArray();
     self.fetchFoodItems = function(category, ids) {
         $.post("/admin/fetchFoodItems", {foodItemIds: JSON.stringify(ids)}, function(items){
             items.forEach(function(item){
-                self.foodItems.push(item);
+                self.foodItems.push(new FoodItemModel(item));
             });
         });
     };
     // Initialize
-    self.fetchFoodItems(category, category.foodItems);
+    if (category) {
+        self.id(category._id);
+        self.name(category.name);
+        self.description(category.description);
+        self.restaurantId(category.restaurantId);
+        if (category.foodItems.length > 0) {
+            self.fetchFoodItems(category, category.foodItems);
+        }
+    }
 }
 
 
-//function FoodItemModel(item){
-//    self.name = item.name;
-//}
+function FoodItemModel(item){
+    var self = this;
+    self.id = ko.observable();
+    self.name = ko.observable();
+    self.price = ko.observable();
+    self.available = ko.observable();
+    self.description = ko.observable();
+    self.imgPath = ko.observable();
+    self.averagePrepTime = ko.observable();
+    self.restaurantId = restaurantId;
+    // Initialize
+    if (item) {
+        self.id(item._id);
+        self.name(item.name);
+        self.price(item.price);
+        self.available(item.available);
+        self.description(item.description);
+        self.imgPath = item.imgPath;
+        self.averagePrepTime = item.averagePrepTime;
+        self.restaurantId = item.restaurantId;
+    }
+}

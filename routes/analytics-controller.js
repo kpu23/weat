ca55b7553f1c855b3e4ab2f188457129c8e44d7f
hlgott1593 <hlgott1593@gmail.com>
2016-04-25 12:05:42 -0400

    added data integration on marketing analytics page

--- routes/analytics-controller.js
+++ routes/analytics-controller.js
@@ -4,6 +4,60 @@
 var express = require('express');
 var router = express.Router();
 var Order = require('../models/order');
+var FoodItem = require("../models/food-item");
+
+// Marketing and Analytics
+router.get('/marketing_analytics', function (req, res) {
+
+    var data = {
+        numbOfOrders: 0,
+        totalSales: 0,
+        topSeller: ''
+    };
+    var itemIdHashmap = {};
+    var restaurantId = req.session.user.restaurantId;
+    var today = new Date();
+    console.log(today);
+    //get daily orders
+    Order.find({}, function(error, orders) {       
+        if (orders) {
+            data.numbOfOrders = orders.length;
+            console.log(orders);   
+
+            for(i = 0; i < orders.length; i++){
+                //add up total sales
+                data.totalSales += orders[i].total;
+                //bucket item counts
+                for(j = 0; j < orders[i].itemIds.length; j++){
+                    
+                    if(itemIdHashmap[orders[i].itemIds[j]]) {
+                        itemIdHashmap[orders[i].itemIds[j]] ++;
+                    } else {
+                        itemIdHashmap[orders[i].itemIds[j]] = 1                       
+                    };
+                }  
+            } 
+            //get top seller
+            var topSellerItemId;
+            var topSellerCount = 0;
+            for(var key in itemIdHashmap){
+                if(!topSellerItemId || itemIdHashmap[key] > topSellerCount){
+                    topSellerItemId = key;
+                }
+            }    
+            FoodItem.findOne({_id: topSellerItemId}, function (error, item){
+                if(item){
+                    data.topSeller = item.name;
+                    console.log(item);
+                }              
+                res.render('marketing_analytics', {title: 'weat: Marketing & Analytics', analyticData: data});
+            });          
+        }
+    });
+    //get daily sales
+    //get daily favorite item
+
+});
 
 router.post('/admin/getDailyOrderData', function (req, res) {
     var restaurantId = req.session.user.restaurantId;
@@ -13,14 +67,14 @@ router.post('/admin/getDailyOrderData', function (req, res) {
         var orderCountByHours = new Array(24); //each index = an hour
         if (orders) {
             orders.forEach(function (order) {
-                console.log(order.submitTime.getHours());
+                //console.log(order.submitTime.getHours());
                 var hour = order.submitTime.getHours();
                 if(!orderCountByHours[hour]) {
                     orderCountByHours[hour] = 1
                 } else {
                     orderCountByHours[hour] ++;
                 };
-                console.log(orderCountByHours);
+                //console.log(orderCountByHours);
             });
         }
         res.send(orderCountByHours);

5ba7226ac795b0f13375d806c965c452e04a4abf
hlgott1593 <hlgott1593@gmail.com>
2016-05-10 15:24:14 -0400

    updated custom reports

--- public/scripts/custom_report.js
+++ public/scripts/custom_report.js
@@ -12,3 +12,30 @@ function demoTwoPageDocument() {
 	doc.save('Report.pdf');
 }
 
+$(function() {
+
+    $('input[name="datefilter"]').daterangepicker({
+        autoUpdateInput: false,
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            },
+        locale: {
+        cancelLabel: 'Clear'
+        }
+    });
+
+    $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+    	$(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+    });
+
+    $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
+    	$(this).val('');
+    });
+    $('input[name="datefilter"]').val(moment() + ' - '+ moment());
+
+});

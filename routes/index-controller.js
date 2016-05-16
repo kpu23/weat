f470d24483bd8450a65cba76fcbb07375478527c
Kevin Utz <kevin.utz@benefitvault.net>
2016-05-16 18:04:55 -0400

    case insensi??

--- routes/index-controller.js
+++ routes/index-controller.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index.ejs', {title: 'weat: home'});
+    res.render('Index.ejs', {title: 'weat: home'});
 });
 
 module.exports = router;

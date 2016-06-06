fcddb9272aa90755d8ff92dc4f2e37323891364b
Kevin Utz <kevin.utz@benefitvault.net>
2016-06-06 17:18:58 -0400

    1

--- public/scripts/custom_report.js
+++ public/scripts/custom_report.js
@@ -1,10 +1,10 @@
 function demoTwoPageDocument() {
-    var sales = parseInt($('#totalSales').val()).toFixed(2);
+    var sales = parseInt($('#totalSales').val()).toFixed(2).text();
 
 	var doc = new jsPDF();
 	doc.text(80, 20, 'Date Range: ' + $('#dateRange').val());
 	doc.text(20, 30, $('#totalOrders').text());
-	doc.text(20, 40, sales.text());
+	doc.text(20, 40, sales);
 	doc.text(20, 50, $('#topSellingItem').text());
 	doc.text(20, 60, $('#dayOfMostOrders').text());
 	doc.text(20, 70, $('#dayOfHighestSales').text());
